62c353e690cf8a60f5b5a26af819a6f7b0c9b59a
FeuerImBlut <golubovich.as@gmail.com>
2022-02-21 16:34:10 +0300

    feat: add features

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const cards = document.querySelectorAll('.card');
+let leaderboardBlock = document.querySelector('.leaderboard');
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 let endTurns = 8;
 let turns = 0;
+let leaderboard;
+
 
 function flipCard() {
     if (lockBoard) return;
@@ -26,7 +29,11 @@ function flipCard() {
 function checkForMatch() {
     let isMatch = firstCard.dataset.dc === secondCard.dataset.dc;
     isMatch ? disableCards() : unflipCards();
-    if (!endTurns) alert(turns);
+    if (!endTurns) {
+        setTimeout(() => {
+            endGame();
+        }, 800);
+    }
 }
 
 function disableCards() {
@@ -60,3 +67,47 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+function setLocalStorage() {
+    localStorage.setItem('leaderboard', leaderboard);
+}
+
+(function getLocalStorage() {
+    if (localStorage.getItem('leaderboard')) {
+        leaderboard = localStorage.getItem('leaderboard');
+    }
+})()
+
+function endGame() {
+    let name = prompt('Enter your name:', 'Joker');
+    let result = `${name}: ${turns}`;
+    if (leaderboard.length < 10) {
+        leaderboard.push(result);
+    }
+    else {
+        leaderboard.shift();
+        leaderboard.push(result);
+    }
+    setLocalStorage();
+    alert(`Congratulations, ${name} ! It took you ${turns} turns to finish the game`);
+}
+
+// window.addEventListener('beforeunload', setLocalStorage);
+// window.addEventListener('load', getLocalStorage);
+
+(function fillLeaderboard() {
+    while (leaderboardBlock.firstChild) {
+        leaderboardBlock.firstChild.remove();
+    }
+    if (leaderboard.length) {
+        leaderboard = [];
+        return;} 
+    leaderboard.forEach(element => {
+        let div = document.createElement('div');
+        div.innerHTML = element;
+        leaderboardBlock.appendChild(div);
+    });
+})()
+
+endGame();
+
